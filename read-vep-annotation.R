39d8df54e20e27a3e44aca83848af41183011c18
Oliver Mazariegos <32202318+Oliversinn@users.noreply.github.com>
2024-10-12 13:56:19 -0400

    feat: parse vep annotations

---
+++ read-vep-annotation.R
@@ -0,0 +1,24 @@
+library(VariantAnnotation)
+
+file_name = ""
+genome_build = "hg38"
+
+# Load VCF
+vcf <- readVcf(file_name, genome_build)
+
+# Get VEP Annotations
+info_data <- info(vcf)
+csq_data <- as.character(unlist(info_data$CSQ))
+csq_list <- strsplit(csq_data, "\\|")
+
+# Extract the header description for the CSQ field
+vcf_header <- header(vcf)
+csq_header <- info(vcf_header)["CSQ", "Description"]
+
+# Parse the CSQ header to get column names
+csq_columns <- strsplit(gsub(".*Format: ", "", csq_header), "\\|")[[1]]
+
+# Bind the split CSQ list into a data frame
+csq_df <- do.call(rbind, csq_list)
+csq_df = as.data.frame(csq_df)
+colnames(csq_df) <- csq_columns
